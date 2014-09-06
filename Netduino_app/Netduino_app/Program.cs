f4b8212cc0a651a84889b3bf882af28240145266
Stephen Stair <sgstair@akkit.org>
2014-09-06 14:51:57 -0700

    Refining code, interfacing multiple sensors correctly.

--- Netduino_app/Netduino_app/Program.cs
+++ Netduino_app/Netduino_app/Program.cs
@@ -13,23 +13,35 @@ namespace Netduino_app
     {
         public static void Main()
         {
-            // write your code here
+            // Store data for delivery to the webservice
+            const int MaxDevices = 32;
+            int deviceCount = 0;
+            byte[] DeviceSerial = new byte[MaxDevices* 8];
+            byte[] DeviceTemperature = new Byte[MaxDevices* 2];
+            bool[] DeviceValid = new bool[MaxDevices];
 
+            // Sensor bus
             OneWire oneWire = new OneWire(new OutputPort(Pins.GPIO_PIN_D13, false));
 
+            // Scratch data for interfacing with the sensor.
             byte[] serialNum = new byte[8];
             byte[] scratchPad = new byte[9];
 
             while(true)
             {
+                deviceCount = 0;
                 int port = oneWire.FindFirstDevice(true, false);
  
                 while(port != 0)
                 {
+                    // Read the serial number
                     oneWire.SerialNum(serialNum, true);
-                    Debug.Print(port.ToString());
-                    Debug.Print(DumpHex(serialNum));
+                    //Debug.Print(port.ToString());
+                    //Debug.Print(DumpHex(serialNum));
+                    Array.Copy(serialNum, 0, DeviceSerial, deviceCount * 8, 8);
+                    DeviceValid[deviceCount] = false;
 
+                    // start temperature conversion
                     oneWire.TouchReset();
                     oneWire.WriteByte(0x55);
                     for (int i = 0; i < 8; i++)
@@ -37,33 +49,62 @@ namespace Netduino_app
                         oneWire.WriteByte(serialNum[i]);
                     }
                     oneWire.WriteByte(0x44);
+
+                    // Wait for temperature conversion to be complete
+                    // This means the exact time of sampling is going to be skewed by about a second per sensor
+                    // (There are other sources of sample time skew too, and I'm not timestamping the data locally to avoid this)
+                    // If we were to do the conversions simultaneously that would be more complex and probably increase noise in the system.
+                    // For my use case this skew is acceptable.
                     Thread.Sleep(850);
 
-                    oneWire.TouchReset();
-                    oneWire.WriteByte(0x55);
-                    for (int i = 0; i < 8; i++)
-                    {
-                        oneWire.WriteByte(serialNum[i]);
-                    }
-                    oneWire.WriteByte(0xBE);
-                    for (int i = 0; i < 9; i++)
-                    {
-                        scratchPad[i] = (byte)oneWire.ReadByte();
-                    }
 
-                    Debug.Print(DumpHex(scratchPad));
-                    byte crc = ComputeCrc(scratchPad, 8);
-                    Debug.Print(crc.ToString("x2"));
+                    // Attempt read 3 times.
+                    for (int retry = 0; retry < 3; retry++)
+                    {
+                        oneWire.TouchReset();
+                        oneWire.WriteByte(0x55);
+                        for (int i = 0; i < 8; i++)
+                        {
+                            oneWire.WriteByte(serialNum[i]);
+                        }
+                        oneWire.WriteByte(0xBE);
+                        for (int i = 0; i < 9; i++)
+                        {
+                            scratchPad[i] = (byte)oneWire.ReadByte();
+                        }
 
-                    port = oneWire.FindNextDevice(false, true);
+                        //Debug.Print(DumpHex(scratchPad));
+                        byte crc = ComputeCrc(scratchPad, 8);
+                        //Debug.Print(crc.ToString("x2"));
+
+                        // If read is valid, store value and stop trying to read.
+                        if (crc == scratchPad[8])
+                        {
+                            DeviceValid[deviceCount] = true;
+                            Array.Copy(scratchPad, 0, DeviceTemperature, deviceCount * 8, 8);
+                            break;
+                        }
+                    }
+                    deviceCount++;
+
+
+                    port = oneWire.FindNextDevice(true, false);
                 }
 
-                break;
+                // Create web request and submit data to webservice.
+
+
+
+                // Wait before sampling again.
+                Thread.Sleep(5000);
             }
 
 
         }
 
+        /// <summary>
+        /// Debugging function to convert a byte array to a hex string.
+        /// </summary>
         public static string DumpHex(byte[] data)
         {
             string[] strings = new string[data.Length];
@@ -74,6 +115,9 @@ namespace Netduino_app
             return string.Concat(strings);
         }
 
+        /// <summary>
+        /// DS18B20 CRC function (8-bit CRC, LSB first, initial vector zero, X^8 + X^5 + X^4 + 1 = 0x(topbit)31, reversed to 0x8C(topbit) )
+        /// </summary>
         public static byte ComputeCrc(byte[] data, int length)
         {
             const byte crc = 0x8C;
